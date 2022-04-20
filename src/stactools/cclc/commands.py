6a8bfea4b8eb4e6aab8880e8da37a942c41cbe9f
pjhartzell <preston.hartzell@gmail.com>
2022-04-20 09:11:27 -0400

    clean up tiler

--- src/stactools/cclc/commands.py
+++ src/stactools/cclc/commands.py
@@ -1,8 +1,9 @@
 import logging
+from typing import Optional
 
 import click
 
-from stactools.cclc.constants import DEFAULT_LOWER_LEFT, DEFAULT_TILE_SIZE
+from stactools.cclc.constants import DEFAULT_LEFT_BOTTOM, DEFAULT_TILE_SIZE
 from stactools.cclc.utils import tile
 
 logger = logging.getLogger(__name__)
@@ -26,15 +27,30 @@ def create_cclc_command(cli):
                   default=DEFAULT_TILE_SIZE,
                   help="Tile size in meters")
     @click.option("-l",
-                  "--lower-left",
-                  default=DEFAULT_LOWER_LEFT,
+                  "--left-bottom",
+                  default=DEFAULT_LEFT_BOTTOM,
                   type=(int, int),
-                  help="Lower left origin of tiles")
-    def tile_command(infile: str, outdir: str, size: int,
-                     lower_left: tuple((int, int))):
+                  help="left, bottom coordinate origin of tiles")
+    @click.option("-n", "--nodata", type=int, help="nodata value")
+    def tile_command(infile: str,
+                     outdir: str,
+                     size: int,
+                     left_bottom: tuple((int, int)),
+                     nodata: Optional[int] = None) -> None:
         """Tiles the input file to a grid.
-        The source CCLC data are huge GeoTIFFS, so we tile the geotiffs.
+
+        The source CCLC data are large GeoTIFFS, so we tile them to COGs.
+
+        \b
+        Args:
+            infile (str): HREF to source GeoTIFF to be tiled
+            outdir (str): Directory that will contain the tiles
+            size (int): Tile size in meters
+            left_bottom (tuple(int, int)): X, Y coordinates of tile grid origin.
+                Defined as the lower left corner of the area to be tiled.
+            nodata (int): nodata value to use for tiled COGs
+
         """
-        tile(infile, outdir, size, lower_left)
+        tile(infile, outdir, size, left_bottom, nodata)
 
     return cclc
--- src/stactools/cclc/utils.py
+++ src/stactools/cclc/utils.py
@@ -1,47 +1,29 @@
 import os.path
+from typing import List, Optional
 
 import rasterio
 from stactools.core.utils.subprocess import call
 
 
-def tile(infile: str, outdir: str, size: int,
-         lower_left: tuple[(int, int)]) -> None:
-    """Tiles the given input to a grid."""
-    with rasterio.open(infile) as dataset:
-        _, _, right, top = dataset.bounds
-        left, bottom = lower_left
-        tiles = create_tiles(left, bottom, right, top, size)
-    for tile in tiles:
-        tile.subset(infile, outdir)
-
-
-def create_tiles(left, bottom, right, top, size):
-    x = left
-    y = bottom
-    tiles = []
-    while x < right:
-        while y < top:
-            tile = Tile(x, y, min(x + size, right), min(y + size, top))
-            tiles.append(tile)
-            y += size
-        x += size
-        y = bottom
-    return tiles
-
-
 class Tile:
 
-    def __init__(self, left, bottom, right, top):
+    def __init__(self,
+                 left: float,
+                 bottom: float,
+                 right: float,
+                 top: float,
+                 nodata: Optional[int] = None):
         self._left = left
         self._bottom = bottom
         self._right = right
         self._top = top
+        self._nodata = nodata
 
-    def subset(self, infile, outdir):
+    def subset(self, infile, outdir) -> None:
         base = os.path.splitext(os.path.basename(infile))[0]
         outfile = os.path.join(
             outdir,
-            (f"{base}_{str(int(self._left))}_{str(int(self._bottom))}.tif"))
+            (f"{base}_E{str(int(self._left))}_N{str(int(self._bottom))}.tif"))
         args = [
             "gdal_translate", "-of", "COG", "-co", "compress=deflate", "-co",
             "blocksize=512", "-projwin",
@@ -50,6 +32,41 @@ class Tile:
             str(self._right),
             str(self._bottom)
         ]
+        if self._nodata:
+            args.extend(["-a_nodata", str(self._nodata)])
         args.append(infile)
         args.append(outfile)
-        return call(args)
+        call(args)
+
+
+def tile(infile: str,
+         outdir: str,
+         size: int,
+         left_bottom: tuple[(int, int)],
+         nodata: Optional[int] = None) -> None:
+    """Tiles the given input to a grid."""
+    with rasterio.open(infile) as dataset:
+        _, _, right, top = dataset.bounds
+        left, bottom = left_bottom
+        tiles = create_tiles(left, bottom, right, top, size, nodata)
+    for tile in tiles:
+        tile.subset(infile, outdir)
+
+
+def create_tiles(left: float,
+                 bottom: float,
+                 right: float,
+                 top: float,
+                 size: int,
+                 nodata: Optional[int] = None) -> List[Tile]:
+    x = left
+    y = bottom
+    tiles = []
+    while x < right:
+        while y < top:
+            tile = Tile(x, y, min(x + size, right), min(y + size, top), nodata)
+            tiles.append(tile)
+            y += size
+        x += size
+        y = bottom
+    return tiles
